70662b4783451ae1a2961741b3f52a1d95c0e89e
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-04-27 15:51:39 -0300

    subida de carpeta y archivo Navbar, junto con la creacion de los enlaces en el proyecto

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#">Ejemplo</a></li>
+            <li><a href="#">Ejemplo2</a></li>
+            <li><a href="#">Ejemplo3</a></li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
