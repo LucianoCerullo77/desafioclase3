c70264f2f9076ad090937d3b5aea465a98ba6cd9
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-04-27 16:02:24 -0300

    Creacion del Navbar con JSX, import y export del mismo en app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <nav>
-          <ul>
-            <li><a href="#">Ejemplo</a></li>
-            <li><a href="#">Ejemplo2</a></li>
-            <li><a href="#">Ejemplo3</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return(
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Ejemplo</a></li>
+                <li><a href="#">Ejemplo2</a></li>
+                <li><a href="#">Ejemplo3</a></li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
+
+export default Navbar
